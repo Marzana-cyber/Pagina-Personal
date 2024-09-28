73c58311e8c5ebb68f2af14c2948ca1eb7f2f1ce
Paola <2003pvmp@gmail.com>
2024-09-27 21:43:58 -0400

    Pagina de inicio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Ofimática" , "Lenguajes de programación" , "Aspirante a medidas de seguridad"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDeplay: 1000,
+    loop: true
+})
