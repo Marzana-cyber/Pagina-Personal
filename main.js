eaf610b0bdb3d4f2e031a5296d5cf0145d14e1ec
Paola <2003pvmp@gmail.com>
2024-09-29 23:12:14 -0400

    Terminando el proyecto

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Ofimática" , "Lenguajes de programación" , "Aspirante a medidas de seguridad"],
+    strings: ["Ofimática" , "Lenguajes de programación básicos"],
     typeSpeed: 100,
     backSpeed: 100,
     backDeplay: 1000,
